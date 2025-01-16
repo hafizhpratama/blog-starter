1842c62ff83d713f38977a0668e9d949452f540b
Hafizh Pratama <hafizhpratama99@gmail.com>
2025-01-16 13:24:23 +0800

    fix: update meta data

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -37,12 +37,6 @@ export async function generateMetadata({
           height: 630,
           alt: meta.title,
         },
-        {
-          url: `${process.env.NEXT_PUBLIC_BASE_URL}/thumbnail.png`,
-          width: 1200,
-          height: 630,
-          alt: meta.title,
-        },
       ],
     },
     twitter: {
